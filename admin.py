5ea49d5dcdccb993821a1512efd47f5a73b41c09
Sebastian Żurek <sebzur@codelab.pl>
2015-01-26 13:06:35 +0100

    PostSaveMessageEvent w adminie

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from models import *
+from base import PostSaveMessageEvent
 from django.contrib import admin
 from django.db.models import get_model
 
@@ -11,8 +12,13 @@ class MessageLogAdmin(admin.ModelAdmin):
     list_display = ('subject', 'message_type', 'recipient', 'to_emails', 'creation_datetime', 'sent_sucessfully', 'trigger_info')
     search_fields = ('subject',)
 
+
+class PostSaveMessageEventAdmin(admin.ModelAdmin):
+    list_display = ('publication_datetime', 'instance_id', 'app_label', 'model_name')
+
 admin.site.register(CustomReceiver)
 admin.site.register(MessageLog, MessageLogAdmin)
+admin.site.register(PostSaveMessageEvent, PostSaveMessageEventAdmin)
 
 import registry
 for reg_cls in registry.classes:
